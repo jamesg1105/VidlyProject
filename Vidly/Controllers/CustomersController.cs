131f1e995b6c67aea4f4bc64f6643ec4b01ef461
james1105 <jamessimmons1105@gmail.com>
2018-04-09 17:21:29 -0500

    Added customer and movie tabs and changes to routes.

---
+++ Vidly/Controllers/CustomersController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // GET: Customers
+        [Route("Customers")]
+        public ActionResult Customers()
+        {
+            var customers = new List<Customer>
+            {
+                new Customer { Id = 1, Name = "Billy Willy" },
+                new Customer { Id = 2, Name = "Willy Nilly" }
+            };
+
+            var viewModel = new CustomerViewModel
+            {
+                Customers = customers
+            };
+
+            return View(viewModel);
+        }
+        [Route("Customers/CustomerDetails/{Id}/{Name}")]
+        public ActionResult Details(int Id, string Name)
+        {
+            return Content(Name);
+        }
+    }
+}
--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -4,19 +4,54 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
+using Vidly.ViewModels;
 
 namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
         // GET: Movies/Random
-        public ActionResult Random()
+        [Route("Movies")]
+        public ActionResult Movies()
         {
-            var movie = new Movie()
-            {
-                Name = "Shrek"
+            var movie = new List<Movie> {
+                new Movie { Id = 1, Name = "Shrek" },
+                new Movie { Id = 2, Name = "Billy Madison"}
             };
-            return View(movie);
+
+            var viewModel = new MovieViewModel
+            {
+                Movie = movie
+            };
+
+            return View(viewModel);
         }
+
+        [Route("Movies/MovieDetails/{Id}/{Name}")]
+        public ActionResult Details(int Id, string Name)
+        {
+            return Content(Name);
+        }
+
+        [Route("movies/released/{year}/{month:regex(\\d{4}):range(1, 12)}")]
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(year + "/" + month);
+        }
+
+        //public ActionResult Edit(int id)
+        //{
+        //    return Content("id=" + id);
+        //}
+
+        ////Movies
+        //public ActionResult Index(int? pageIndex, string sortBy)
+        //{
+        //    if (!pageIndex.HasValue)
+        //        pageIndex = 1;
+        //    if (String.IsNullOrEmpty(sortBy))
+        //        sortBy = "Name";
+        //    return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+        //}
     }
 }
